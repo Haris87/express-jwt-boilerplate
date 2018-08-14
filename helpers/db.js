8c795201f3d067fba542d43db2fa20d72a17a426
alex--pg <alex.ppg@protonmail.com>
2018-08-14 22:23:49 +0300

    Optimizations & Colored output

--- helpers/db.js
+++ helpers/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const chalk = require("chalk");
 require("dotenv").config();
 
 const connectionURL = process.env.CONNECTION_URL;
 
-function connect() {
+const connect = () => {
   mongoose.connect(
     connectionURL,
     {
@@ -13,18 +14,19 @@ function connect() {
       }
     }
   );
-
-  console.log("db connected.");
+  // The substring essentially gets the actual URL of the DB
+  console.log(`${chalk.green('MongoDB')}: Successfully connected to ${connectionURL.substring(connectionURL.indexOf('@') + 1, connectionURL.indexOf(':', connectionURL.indexOf('@')))}`);
 }
 
-function errorHandler(schema) {
+const errorHandler = () => {
   schema.post("save", (error, doc, next) => {
     if (error.name === "MongoError" && error.code === 11000) {
       const field = error.message.split(".$")[1].split("_1")[0];
       const value = error.message.split("{ : ")[1].split(" }")[0];
+      console.log(`${chalk.green('MongoDB')}: Error ${error.code} - An entry with ${field} ${value} already exists.`);
       next({
         status: 409,
-        error: "An entry with " + field + " " + value + " already exists."
+        error: `An entry with ${field} ${value} already exists.`
       });
     } else {
       next();
@@ -32,9 +34,7 @@ function errorHandler(schema) {
   });
 }
 
-const db = {
+module.exports = {
   connect: connect,
   errorHandler: errorHandler
-};
-
-module.exports = db;
+}
