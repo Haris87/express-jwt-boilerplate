f52db11f5692d73c627e0f812e2f376ab513f6e6
alex--pg <alex.ppg@protonmail.com>
2018-08-14 22:29:01 +0300

    ES optimizations

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -6,20 +6,19 @@ require("dotenv").config();
  * Used in restricted routes.
  * If JWT is given, passes user object in request, else response with 401 Unauthorized.
  */
-let middleware = function(req, res, next) {
+const middleware = (req, res, next) => {
   let jwtToken;
 
   try {
     jwtToken = req.headers.authorization.split("Bearer ")[1];
   } catch (e) {
     // if not provided
-    res.status(401).send({
+    return res.status(401).send({
       error: "Unauthorized request."
     });
-    return;
   }
 
-  let decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
+  const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
 
   if (decoded) {
     req.user = decoded.user;
