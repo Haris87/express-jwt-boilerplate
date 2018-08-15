f1e992126d8635a025d68a8845bcf8811a6bade3
alex--pg <alex.ppg@protonmail.com>
2018-08-15 16:01:50 +0300

    Minor fix on color

--- helpers/db.js
+++ helpers/db.js
@@ -15,7 +15,7 @@ const connect = () => {
     }
   );
   // Regex with positive look-ahead and look-behind to extract URL
-  console.log(`${chalk.brightGreen('MongoDB')}: Successfully connected to ${connectionURL.match(/(?<=@).*(?=:)/g)}`);
+  console.log(`${chalk.greenBright('MongoDB')}: Successfully connected to ${connectionURL.match(/(?<=@).*(?=:)/g)}`);
 }
 
 const errorHandler = (schema) => {
@@ -28,19 +28,19 @@ const errorHandler = (schema) => {
           status: 409,
           error: `An entry with ${field} ${value} already exists`
         });
-        console.log(`${chalk.brightGreen('MongoDB')}: Error ${error.code} - An entry with ${field} ${value} already exists`);
+        console.log(`${chalk.greenBright('MongoDB')}: Error ${error.code} - An entry with ${field} ${value} already exists`);
       } else if (error.code === 6 || error.code === 7 || error.code === 89 || error.code === 9001) {
         next({
           status: 503,
           error: `Unable to reach database instance`
         });
-        console.log(`${chalk.brightGreen('MongoDB')}: Error ${error.code} - Unable to reach MongoDB instance`);
+        console.log(`${chalk.greenBright('MongoDB')}: Error ${error.code} - Unable to reach MongoDB instance`);
       } else {
         next({
           status: 500,
           error: `Internal server error`
         });
-        console.log(`${chalk.brightGreen('MongoDB')}: Error ${error.code} - Consult error_codes.err on mongodb's Github`);
+        console.log(`${chalk.greenBright('MongoDB')}: Error ${error.code} - Consult error_codes.err on mongodb's Github`);
       }
     } else {
       next();
